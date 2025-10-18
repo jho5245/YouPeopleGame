adf95dc33d5b14b552d6f21b3c4c95ba43bde094
jho5245 <jho5245@naver.com>
2025-10-18 17:04:39 +0900

    optimize import

--- src/main/java/me/jho5245/youpeoplegame/listener/OpenWindowMerchant.java
+++ src/main/java/me/jho5245/youpeoplegame/listener/OpenWindowMerchant.java
@@ -1,7 +1,6 @@
 package me.jho5245.youpeoplegame.listener;
 
 import com.jho5245.cucumbery.events.addon.protocollib.OpenWindowMerchantEvent;
-import com.jho5245.cucumbery.events.addon.protocollib.WindowItemsEvent;
 import me.jho5245.youpeoplegame.util.ItemLoreModifier;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
